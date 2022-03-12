1f85e0be51cb20e0ff217c0ac7224ca79fc27e3b
Kieran Holroyd <kieran@nitrexdesign.co.uk>
2022-03-12 18:39:43 +0000

    Improved performance, better benchmarking tools, removed a couple useless flags

--- src/execute.ts
+++ src/execute.ts
@@ -10,8 +10,11 @@ import { table } from "cli-ux/lib/styled/table";
 import { FileInfo, FilesystemError } from "./types";
 import { opendir_error } from "./errors";
 import fastFolderSize from "fast-folder-size";
+import { sync as delsync } from "rimraf";
+import { stdout } from "process";
 
-// const isDebug = Boolean(process.env.DEBUG)
+const isDebug = Boolean(process.env.DEBUG);
+const isVerbose = Boolean(process.env.VERBOSE);
 function isValidError(x: any): x is Error {
   return typeof x.message === "string";
 }
@@ -24,6 +27,7 @@ class Execute extends Command {
   module_path_list: FileInfo[] = [];
 
   counted = 0;
+  errors = 0;
 
   remove_vendor = false;
 
@@ -42,39 +46,37 @@ class Execute extends Command {
       char: "d",
       default: 5,
     }),
-    size: flags.boolean({
-      char: "s",
-      description: "calculate folder & total sizes (takes a minute)",
-    }),
-    vendor: flags.boolean({ description: "clean php composer too." }),
   };
 
   public async run() {
     const { flags } = this.parse(Execute);
 
-    this.remove_vendor = flags.vendor;
-
+    console.log(`Searching ${path.resolve(flags.path)} for the modules.`);
     cli.action.start(
       "Thinking",
       `Searching ${path.resolve(flags.path)} for the modules.`,
       { stdout: true }
     );
-    // if (isDebug) console.time("directory_search")
 
-    await this.execDirectorySearch(flags.path, 0, flags.depth);
+    await this.timeIfTrue(
+      isDebug,
+      "directory_search",
+      this.execDirectorySearch.bind(this),
+      [flags.path, 0, flags.depth]
+    );
 
-    // if (isDebug) console.timeEnd("directory_search")
-
-    if (flags.size) {
-      cli.action.start("Thinking", "Calculating size (might take a moment)", {
-        stdout: true,
-      });
-      console.time("full_size_calc");
-      await this.calculateSize();
-      console.timeEnd("full_size_calc");
-    }
-
-    // if (isDebug) console.time("build_table")
+    console.log(
+      `Found ${this.module_path_list.length} directories. calculating size.`
+    );
+    cli.action.start("Thinking", "calculating space savings", {
+      stdout: true,
+    });
+    await this.timeIfTrue(
+      isDebug,
+      "calculate_size",
+      this.calculateSize.bind(this),
+      []
+    );
 
     const tableColumnConfig: () => table.Columns<FileInfo> = () => {
       const base: any = {
@@ -82,43 +84,38 @@ class Execute extends Command {
           header: `Folder path (${this.module_path_list.length} Items)`,
         },
       };
-      if (flags.size)
-        base.size = {
-          header: "Size on disk",
-          get: (row: any) =>
-            this.genDirSize(row, row.info?.size < 1e7 ? "KB" : "MB"),
-        };
+      base.size = {
+        header: "Size on disk",
+        get: (row: any) => this.readableSize(row.info?.size || 0),
+      };
       return base;
     };
 
     cli.action.stop(`done.`);
     cli.table(this.module_path_list, tableColumnConfig());
 
-    // if (isDebug) console.timeEnd("build_table")
     let total = 0;
     for (const fullFile of this.module_path_list) {
       if (fullFile.info.size) total += fullFile.info.size;
     }
     this.log(
-      `Total: ${
-        flags.size
-          ? (total / 1024 / 1024 / 1024).toFixed(2) + "GB"
-          : "not calculated"
-      } -- Searched ${this.counted.toFixed(0)} files`
+      `Total: ${this.readableSize(total)} -- Searched ${this.counted.toFixed(
+        0
+      )} files -- Experienced ${this.errors.toFixed(0)} errors. ${
+        this.errors > 0 ? "run with VERBOSE=true to view errors." : ""
+      }`
     );
     // TODO: allow selection of modules to delete
     this.log(
       `WARNING: Your files will be permenantly deleted, double check the list.`
     );
     if (
-      await cli.confirm("Confirm delete all node_modules found above? (yes/no)")
+      await cli.confirm("Confirm delete all node_modules found above? (y/n)")
     ) {
       await this.execDirectoryDelete(this.module_path_list);
       this.log(
         `Completed, your node_modules have been cleaned${
-          flags.size
-            ? " and you saved " + (total / 1024 / 1024 / 1024).toFixed(2) + "GB"
-            : ""
+          " and you saved " + this.readableSize(total)
         }`
       );
     } else {
@@ -140,7 +137,7 @@ class Execute extends Command {
           if (directory.isDirectory() && fs.existsSync(full_path)) {
             if (
               directory.name === "node_modules" ||
-              (this.remove_vendor && directory.name === "vendor")
+              directory.name === "vendor"
             ) {
               this.module_path_list.unshift({
                 path: full_path,
@@ -154,58 +151,73 @@ class Execute extends Command {
               max_depth
             );
           }
-          this.counted++;
+          this.count();
         }
       }
     } catch (e) {
-      if (isValidError(e)) {
+      if (isVerbose && isValidError(e)) {
         if (isFileError(e)) opendir_error(this, e);
-        this.debug(e);
+        if (!isFileError(e)) console.error(e);
+      } else {
+        this.countError();
       }
     }
   }
 
+  count() {
+    this.counted = this.counted + 1;
+  }
+  countError() {
+    this.errors = this.errors + 1;
+  }
+
   async execDirectoryDelete(directories: FileInfo[]) {
+    this.log(`Deleting ${directories.length} directories`);
     const progression = cli.progress();
     progression.start(directories.length, 0);
-    // cli.action.start(`deleting node_modules`, `Initialising`, {stdout: true})
     await Promise.all(
       directories.map(async (dir) => {
         if (!fs.existsSync(dir.path)) return;
+        delsync(dir.path);
         progression.increment();
-        fs.rmSync(dir.path, { recursive: true });
       })
     );
-    // for await (const dir of directories) {
-    //   // cli.action.start(`deleting node_modules`, `${dir.path} ${this.genDirSize(dir)}`, {stdout: true})
-
-    //   progression.increment()
-    //   fs.rmdirSync(dir.path, {recursive: true})
-    // }
-    // cli.action.stop(`done`)
     progression.stop();
     return true;
   }
 
-  genDirSize(dir: FileInfo, unit = "MB") {
-    if (dir.info?.size) {
-      switch (unit) {
-        case "KB":
-          return "[" + (dir.info.size / 1024).toFixed(0) + " KB]";
+  readableSize(size: number) {
+    let correct_size = 0,
+      unit = this.decideUnit(size);
 
-        case "MB":
-          return "[" + (dir.info.size / 1024 / 1024).toFixed(1) + " MB]";
-
-        case "GB":
-          return "[" + (dir.info.size / 1024 / 1024 / 1024).toFixed(2) + " GB]";
-      }
+    switch (unit) {
+      case "TB":
+        correct_size = size / 1e12;
+        break;
+      case "GB":
+        correct_size = size / 1e9;
+        break;
+      case "MB":
+        correct_size = size / 1e6;
+        break;
+      case "KB":
+        correct_size = size / 1e3;
+        break;
+      default:
+        correct_size = size;
     }
 
-    return "[undefined size]";
+    return `[${correct_size.toFixed(1)} ${unit}]`;
+  }
+
+  decideUnit(size: number) {
+    if (size < 1e6) return "KB";
+    if (size < 1e9) return "MB";
+    if (size < 1e12) return "GB";
+    return "TB";
   }
 
   async calculateSize() {
-    // if (isDebug) console.time("full_size_calc")
     const getFolderSize = util.promisify(fastFolderSize);
 
     for await (const fullFile of this.module_path_list) {
@@ -220,8 +232,49 @@ class Execute extends Command {
       }
       fullFile.info.size = folderSizeFull;
     }
+  }
 
-    // if (isDebug) console.timeEnd("full_size_calc")
+  /** This is a debug function. enable with DEBUG=true before running fsnuke
+   *  to see the time it takes to run for a given function.
+   * 
+   *  ```sh-session
+   * $ DEBUG=true node ./bin/run
+   * directory_search: 0.608ms
+   * full_size_calc: 0.218ms
+    ```
+   *
+   * ---
+   *  For example:
+   *  ```ts
+   *  this.timeIfTrue(isDebug, "function_name", this.function_name.bind(this), [args])
+   *  ```
+   *
+   *  expected console output:
+   *  ```sh-session
+   *  function_name: 0.000ms
+   *  ```
+   *
+   * ---
+   *
+   * @param condition - condition to check
+   * @param time_name used as `console.time(time_name)` and `console.timeEnd(time_name)`
+   * @param func Async Funtion to execute and time if condition is true. NOTE: if running with a fucntion inside a class pass as `this.my_function.bind(this)` to preserve `this` in the function.
+   * @param func_args Arguments to pass to the function (MUST BE ARRAY in order to be passed to function as `func(...func_args)`)
+   * @returns 
+   */
+  async timeIfTrue<T>(
+    condition: boolean,
+    time_name: string,
+    func: (...args: any[]) => Promise<T>,
+    func_args: any[]
+  ): Promise<T> {
+    if (!condition) return await func(...func_args);
+
+    console.time(time_name);
+    let result = await func(...func_args);
+    stdout.write(`\u001b[36m\u001b[4m!DEBUG_TIMING!\u001b[0m `);
+    console.timeEnd(time_name);
+    return result;
   }
 }
 
